fe0f0d46dd4ee15a7d5457ba02d865c281ce16d6
burakovas <burakovas@gmail.com>
2019-01-07 18:39:00 +0300

    yii2_3_1

---
+++ basic/config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+
+    'dsn' => 'mysql:host=localhost;dbname=task_db',
+    'username' => 'geek29',
+    'password' => '123123',
+
+    'charset' => 'utf8',
+
+    //'dsn' => 'mysql:host=10.8.0.1;dbname=task_db',
+    //'username' => 'glebbas1',
+    //'password' => 'Zond',
+
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
